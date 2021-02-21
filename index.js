ec4293af88cbddb0496a8578c2254dce52c2b89e
ujjwaltyagi355 <ujjwaltyagi355@gmail.com>
2021-02-21 13:58:08 +0530

    first

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 /*For the First Dice*/
 var randomNumber1=Math.floor(Math.random()*6) + 1;
-if(randomNumber1>=1 && randomNumber1<2)
-          Document.querySelector(".img1").setAttribute("href","images\dice1.png");
-else if(randomNumber1>=2 && randomNumber1<3)
-          Document.querySelector(".img1").setAttribute("href","images\dice2.png");
-else if(randomNumber1>=3 && randomNumber1<4)
-          Document.querySelector(".img1").setAttribute("href","images\dice3.png");
-else if(randomNumber1>=4 && randomNumber1<5)
-          Document.querySelector(".img1").setAttribute("href","images\dice4.png");
-else if(randomNumber1>=5 && randomNumber1<6)
-          Document.querySelector(".img1").setAttribute("href","images\dice5.png");
-else
-          Document.querySelector(".img1").setAttribute("href","images\dice6.png");
+var randomDiceImage="images/dice"+ randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+
+
+/*For the Sencond Dice*/
+var randomNumber2=Math.floor(Math.random()*6) + 1;
+var randomDiceImage="images/dice"+ randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage);
