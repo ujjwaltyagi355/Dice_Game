abc505d9d5327a5f016975d50b77757fb8b806ce
ujjwaltyagi355 <ujjwaltyagi355@gmail.com>
2021-02-21 14:43:53 +0530

    fourth

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
 var randomNumber2=Math.floor(Math.random()*6) + 1;
 var randomDiceImage="images/dice"+ randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage);
+
+/*Selecting A Winner*/
+
+/*If Player A Wins*/
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML="🥳 Player 1 Wins!!";
+}
+
+/*If Player 2 Wins*/
+else if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML="Player 2 Wins!! 🥳";
+}
+
+/*If It's a Draw*/
+else{
+  document.querySelector("h1").innerHTML="It's A Draw!";
+}
